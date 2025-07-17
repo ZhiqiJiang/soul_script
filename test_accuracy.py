b9bbb3cd8126193e804413fbf5e8719136213c76
jiangzhiqi <jiangzhiqi@soulapp.cn>
2025-07-17 10:44:10 +0000

    token级精度测试脚本支持Triton TensorRT-LLM

--- test_accuracy.py
+++ test_accuracy.py
@@ -1,34 +1,69 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import requests
 import os
+from transformers.generation.utils import GenerationConfig
 
 os.environ["VLLM_USE_V1"] = "0"
 
 
 class Accuracy:
 
-    def __init__(self, model_path, device, init_hf=True):
+    def __init__(self, model_path, device, init_hf=True, is_tensorrt_llm=False, dtype_hf="auto"):
         self.model_path = model_path
         self.device = device
         if init_hf:
-            self.model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype="auto",
+            self.model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=dtype_hf,
                                                               device_map=None).to(device)
         self.tokenizer = AutoTokenizer.from_pretrained(model_path)
+        self.is_tensorrt_llm = is_tensorrt_llm
 
     def hf_generate(self, prompt, max_tokens=20):
-        messages = [{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": prompt}]
-        text = self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-        model_inputs = self.tokenizer([text], return_tensors="pt").to(self.device)
-
-        generated_ids = self.model.generate(model_inputs.input_ids, max_new_tokens=max_tokens, top_k=1)
+        generation_config = GenerationConfig(
+            top_k=1, temperature=1, max_length=4096,
+            max_new_tokens=max_tokens, repetition_penalty=1.0,
+            early_stopping=False, do_sample=True, num_beams=1, top_p=1, pad_token_id=0, eos_token_id=2
+        )
+        model_inputs = self.tokenizer([prompt], return_tensors="pt").to(self.device)
+        generated_ids = self.model.generate(model_inputs.input_ids, generation_config=generation_config)
         generated_ids = [
             output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
         ]
-
-        response = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-        return response
-
-    def send_request_chat(self, prompt, max_tokens=20):
+        out_text = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        return out_text
+    
+    def request_trtllm(self, prompt, max_tokens=20):
+        url = "http://localhost:8000/v2/models/tensorrt_llm_bls/generate"
+        data = {
+            "text_input": prompt,
+            "max_tokens": max_tokens,
+            "top_k": 1
+        }
+        headers = {"Content-Type": "application/json"}
+        response = requests.post(url, headers=headers, json=data)
+        response_json = response.json()
+        out_text = response_json["text_output"]
+        return out_text
+    
+    def request_vllm_completions(self, prompt, max_tokens=20):
+        model_name = "Qwen2-7B-Instruct"
+        data = {
+            "model": model_name,
+            "prompt": prompt,
+            "max_tokens": max_tokens,
+            "top_k": 1,
+            "seed": 41,
+            "top_p": 1,
+            "temperature": 1.0,
+            "repetition_penalty": 1.0
+        }
+        url = "http://localhost:8010/v1/completions"
+        headers = {"Content-Type": "application/json"}
+        response = requests.post(url, headers=headers, json=data)
+        response_json = response.json()
+        out_text = response_json["choices"][0]["text"]
+        return out_text
+    
+    def request_vllm_chat(self, prompt, max_tokens=20):
         model_name = "Qwen2-7B-Instruct"
         data = {
             "model": model_name,
@@ -39,6 +74,9 @@ class Accuracy:
             "max_tokens": max_tokens,
             "top_k": 1,
             "seed": 41,
+            "top_p": 1,
+            "temperature": 1.0,
+            "repetition_penalty": 1.0
         }
         url = "http://localhost:8010/v1/chat/completions"
         headers = {"Content-Type": "application/json"}
@@ -47,20 +85,21 @@ class Accuracy:
         out_text = response_json.get("choices", [{}])[0].get("message", {}).get("content", "")
         return out_text
 
+
+    def send_request_chat(self, prompt, max_tokens=20):
+        if self.is_tensorrt_llm:
+            prompt = self.apply_chat_template(prompt)
+            out_text = self.request_trtllm(prompt, max_tokens)
+        else:
+            out_text = self.request_vllm_chat(prompt, max_tokens)
+        return out_text
+
+
     def send_request_completions(self, prompt, max_tokens=20):
-        model_name = "Qwen2-7B-Instruct"
-        data = {
-            "model": model_name,
-            "prompt": prompt,
-            "max_tokens": max_tokens,
-            "top_k": 1,
-            "seed": 41,
-        }
-        url = "http://localhost:8010/v1/completions"
-        headers = {"Content-Type": "application/json"}
-        response = requests.post(url, headers=headers, json=data)
-        response_json = response.json()
-        out_text = response_json["choices"][0]["text"]
+        if self.is_tensorrt_llm:
+            out_text = self.request_trtllm(prompt, max_tokens)
+        else:
+            out_text = self.request_vllm_completions(prompt, max_tokens)
         return out_text
 
     def test_chat(self, prompt, num_samples=10):
@@ -94,13 +133,14 @@ class Accuracy:
         # print(words)
         return words
 
-    def accuracy(self, prompt):
+    def accuracy(self, prompt, enable_chat_template=True):
+        if enable_chat_template:
+            prompt = self.apply_chat_template(prompt)
         out_text_hf = self.hf_generate(prompt)
         words_hf = self.split_text(out_text_hf)
         hit = 0
         miss = 0
         words_vllm = []
-        prompt = self.apply_chat_template(prompt)
         for word in words_hf:
             out_word_vllm = self.send_request_completions(prompt, 1)
             words_vllm.append(out_word_vllm)
@@ -114,15 +154,15 @@ class Accuracy:
         print("words_vllm:", words_vllm)
         return hit, miss
 
-    def get_texts(self, json_path):
+    def get_texts(self, json_path, text_key='context'):
         import json
         texts = []
         with open(json_path, 'r', encoding='utf-8') as f:
             for line in f:
                 try:
                     obj = json.loads(line)
-                    if 'context' in obj:
-                        texts.append(obj['context'])
+                    if text_key in obj:
+                        texts.append(obj[text_key])
                 except Exception:
                     continue
         return texts
@@ -130,22 +170,28 @@ class Accuracy:
 
 if __name__ == "__main__":
     model_path = "/root/models/Qwen2-7B-Instruct"
-    device = "cuda:2"
+    device = "cuda:5"
     init_hf = True
-    accuracy_tester = Accuracy(model_path, device, init_hf)
+    is_tensorrt_llm = False
+    dtype_hf = "float16"
+    accuracy_tester = Accuracy(model_path, device, init_hf, is_tensorrt_llm, dtype_hf)
     prompt = "如何复现deepseek r1中的知识蒸馏"
 
-    texts = accuracy_tester.get_texts("/root/repo/soul-llm-evaluate/examples/westworld/npc_test_200.json")
+    texts = accuracy_tester.get_texts("/root/repo/soul-llm-evaluate/examples/westworld/npc_test_200.json", "context")
+    # texts = accuracy_tester.get_texts("/root/repo/soul-llm-evaluate/examples/qwen2_200.json", "message")
     # prompt = texts[0]
     # accuracy_tester.test_chat2(prompt)
     # accuracy_tester.test_chat(prompt, 1)
 
     hits = []
     misses = []
+    enable_chat_template = True
     for prompt in texts:
-        hit, miss = accuracy_tester.accuracy(prompt)
+        hit, miss = accuracy_tester.accuracy(prompt, enable_chat_template)
         hits.append(hit)
         misses.append(miss)
     print("Total Hits:", sum(hits))
     print("Total Misses:", sum(misses))
-    print(f"Accuracy: {sum(hits) / (sum(hits) + sum(misses)):.2f}")
+    print(f"Accuracy: {sum(hits) / (sum(hits) + sum(misses)):.4f}")
+
+# vllm serve /root/models/Qwen2-7B-Instruct --served-model-name Qwen2-7B-Instruct --no-enable-prefix-caching --port 8010 --dtype float16
